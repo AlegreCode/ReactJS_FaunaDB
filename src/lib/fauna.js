07d06ef556ba9f56e2eebec59dcfdc48e31a850a
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-02-24 17:35:47 -0300

    Updated fauna.js
    - Creamos el cliente de fauna para conectar la app con la base de datos.

---
+++ src/lib/fauna.js
@@ -0,0 +1,8 @@
+import { Client, fql } from "fauna"
+
+// Asegúrate de crear un archivo .env para almacenar tu clave secreta
+const client = new Client({
+  secret: process.env.VITE_REACT_APP_FAUNA_SECRET,
+})
+
+export default client
